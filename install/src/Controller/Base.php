1fc27a41e0cfdc94ad64cd487a6b68a776893f2f
zhangjincheng <zhangjincheng@huidr.com>
2019-06-19 09:44:26 +0800

    脚手架工程

--- install/src/Controller/Base.php
+++ install/src/Controller/Base.php
@@ -1,13 +1,13 @@
 <?php
+
 namespace App\Controller;
 
 use ESD\Go\GoController;
-use ESD\Plugins\AnnotationsScan\Annotation\Component;
+use ESD\Plugins\Cache\GetCache;
 use ESD\Plugins\EasyRoute\GetHttp;
 use ESD\Plugins\Mysql\GetMysql;
 use ESD\Plugins\Redis\GetRedis;
 use ESD\Plugins\Session\GetSession;
-use ESD\Plugins\Cache\GetCache;
 
 
 /**
